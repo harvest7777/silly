9bf5151ca09db4e8b7a3f87ccbf7e92d77d17029
xiuv <333harvest.moon333@gmail.com>
2024-11-17 12:03:31 -0800

    made client sided signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import CreateClientTest from "../../testing/create_client_test";
+import SignUpPage from "../../auth/signup/signup";
 export default function Home() {
   return (
     <div> 
       <h1>This is going to be the home page</h1>
       <CreateClientTest/>
+      <SignUpPage/>
     </div>
   );
 }
